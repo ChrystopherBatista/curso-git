a82bce0f68766ddb02870b8cb65aa10c2c60d6f3
Chrystopher Batista <chrystopherb@gmail.com>
2018-06-11 09:40:07 -0300

    Projeto iniciado mostrando a mensagem

---
+++ Projeto-teste-git/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_teste_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Marie!");
+
+            Console.ReadLine();
+        }
+    }
+}
